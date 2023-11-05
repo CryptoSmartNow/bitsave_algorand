69fa4a4399d9f4e71fe7a3629600a9aba8d5cdb0
xpanvictor <xpanvictor@gmail.com>
2023-11-05 23:58:44 +0100

    feat(contract): compiles and ready for other tests

--- backend/smart_contracts/helpers/build.py
+++ backend/smart_contracts/helpers/build.py
@@ -2,11 +2,20 @@ import logging
 import subprocess
 from pathlib import Path
 from shutil import rmtree
+from algokit_utils import ApplicationClient, get_algod_client
+from algosdk.v2client.algod import AlgodClient
+from dotenv import load_dotenv
 
 import beaker
 
 logger = logging.getLogger(__name__)
 deployment_extension = "py"
+
+# ----------++ To build this localnet
+env_path = Path(__file__).parent.parent.parent / ".env.localnet"
+load_dotenv(env_path)
+algodClient = get_algod_client()
+# ------------ To build this localnet
 
 
 def build(output_dir: Path, app: beaker.Application) -> Path:
@@ -15,7 +24,7 @@ def build(output_dir: Path, app: beaker.Application) -> Path:
         rmtree(output_dir)
     output_dir.mkdir(exist_ok=True, parents=True)
     logger.info(f"Exporting {app.name} to {output_dir}")
-    specification = app.build()
+    specification = app.build(client=algodClient)
     specification.export(output_dir)
 
     result = subprocess.run(
